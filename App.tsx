492ac816aef076f9704200526d3372044e0165e6
shravanshelar652-bit <shravanshelar652@gmail.com>
2026-05-13 15:22:14 +0530

    Create App component with alert message

---
+++ App.tsx
@@ -0,0 +1,11 @@
+export default function App() {
+  return (
+    <div style={{
+      padding: "40px",
+      fontSize: "30px",
+      textAlign: "center"
+    }}>
+      Sarkari Yojana Alert Working
+    </div>
+  );
+}
